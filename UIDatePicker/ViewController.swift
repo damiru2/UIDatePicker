bed28f41221d3c14c5d4675dd9e11c7d88c5f6ab
damiru2 <chalkarov11@mail.ru>
2022-07-20 21:58:06 +0500

    created data picker by code

--- UIDatePicker/ViewController.swift
+++ UIDatePicker/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var picker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        picker.preferredDatePickerStyle = .wheels
+        picker.datePickerMode = .dateAndTime
+        picker.minuteInterval = 5
     }
 
 
+    @IBAction func pickerAction(_ sender: UIDatePicker) {
+        print(sender.date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMM d, yyyy HH:mm:ss"
+        formatter.timeZone = TimeZone.current
+        print(formatter.string(from: sender.date))
+        
+    }
+    
+    
 }
 
